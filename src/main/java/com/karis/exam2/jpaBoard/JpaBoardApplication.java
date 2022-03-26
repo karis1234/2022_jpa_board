e48af1e64ac495df84efd72b339df4fdbd7eb005
karis1234 <qkzkf3487@naver.com>
2022-03-26 16:59:18 +0900

    세팅

---
+++ src/main/java/com/karis/exam2/jpaBoard/JpaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.karis.exam2.jpaBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBoardApplication.class, args);
+	}
+
+}
