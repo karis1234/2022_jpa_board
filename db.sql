859b99f3b0c1dc4e1813c8c7fc38bf5fccea761e
karis1234 <qkzkf3487@naver.com>
2022-03-26 17:32:47 +0900

    게시물 table 추가(article)

---
+++ db.sql
@@ -0,0 +1,30 @@
+CREATE TABLE article(
+    id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    reg_date DATETIME NOT NULL,
+    update_date DATETIME NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    `body` TEXT NOT NULL,
+    `user_id` BIGINT UNSIGNED NOT NULL
+);
+
+# 회원데이터 생성
+INSERT INTO article
+SET reg_date = NOW(),
+update_date = NOW(),
+title = '제목1',
+`body` = '내용1',
+`user_id` = '1';
+
+INSERT INTO article
+SET reg_date = NOW(),
+update_date = NOW(),
+title = '제목2',
+`body` = '내용2',
+`user_id` = '2';
+
+INSERT INTO article
+SET reg_date = NOW(),
+update_date = NOW(),
+title = '제목2',
+`body` = '내용2',
+`user_id` = '2';
