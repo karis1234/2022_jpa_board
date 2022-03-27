91ea87634301b8a7b7ce4acd75f6e16f86ea8ed7
karis1234 <qkzkf3487@naver.com>
2022-03-27 17:19:51 +0900

    게시물 삭제 및 게시물 수정 시 updateDate 최신화

--- src/main/java/com/karis/exam2/user/controller/UserController.java
+++ src/main/java/com/karis/exam2/user/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.swing.text.html.Option;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,17 +32,27 @@ public class UserController {
     }
     @RequestMapping("doModify")
     @ResponseBody
-    public Article showModify(long id, String title, String body) {
+    public Article doModify(long id, String title, String body) {
         Article article = articleRepository.findById(id).get();
         if(title != null){
             article.setTitle(title);
+            article.setUpdateDate(LocalDateTime.now());
         }
         if(body != null){
             article.setBody(body);
+            article.setUpdateDate(LocalDateTime.now());
         }
         articleRepository.save(article);
         return article;
     }
+    @RequestMapping("doDelete")
+    @ResponseBody
+    public String showDelete(long id) {
+        articleRepository.deleteById(id);
+        return "%d번 게시물이 삭제되었습니다.".formatted(id);
+    }
+
+
 
 
 
