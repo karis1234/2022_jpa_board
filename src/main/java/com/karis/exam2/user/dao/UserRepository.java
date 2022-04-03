f6a588bc9e525c92e46b1eb9a72aa8f403bf684e
karis1234 <qkzkf3487@naver.com>
2022-04-03 15:38:31 +0900

    로그인 기능 구현

--- src/main/java/com/karis/exam2/user/dao/UserRepository.java
+++ src/main/java/com/karis/exam2/user/dao/UserRepository.java
@@ -3,6 +3,11 @@ package com.karis.exam2.user.dao;
 import com.karis.exam2.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+    // 가져올게 1가지면 Optional로 감싼다.
 }
