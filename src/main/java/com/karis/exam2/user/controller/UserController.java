ba21f42196c7b7afdfc520bb433fe98c3e02c5da
karis1234 <qkzkf3487@naver.com>
2022-04-03 16:52:54 +0900

    쿠키를 이용한 유저 정보 기억git add .

--- src/main/java/com/karis/exam2/user/controller/UserController.java
+++ src/main/java/com/karis/exam2/user/controller/UserController.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -56,7 +60,7 @@ public class UserController {
     }
     @RequestMapping("doLogin")
     @ResponseBody
-    public String doLogin(String email, String password){
+    public String doLogin(String email, String password, HttpServletRequest req, HttpServletResponse resp){
         if ( email == null || email.trim().length() == 0 ) {
             return "이메일을 입력해주세요.";
         }
@@ -80,12 +84,33 @@ public class UserController {
             return "비밀번호가 일치하지 않습니다.";
         }
 
+        Cookie cookie = new Cookie("loginedUserId", user.get().getId() + "");
+        resp.addCookie(cookie);
+
         return "%s님 환영합니다.".formatted(user.get().getName());
     }
     @RequestMapping("me")
     @ResponseBody
-    public User showMe(String email, String password){
-        Optional<User> user = userRepository.findById(1L);
+    public User showMe(HttpServletRequest req){
+        boolean isLogined = false;
+        long loginedUserId = 0;
+
+        Cookie[] cookies = req.getCookies();
+
+        if ( cookies != null ) {
+            for ( Cookie cookie : cookies ) {
+                if ( cookie.getName().equals("loginedUserId") ) {
+                    isLogined = true;
+                    loginedUserId = Long.parseLong(cookie.getValue());
+                }
+            }
+        }
+
+        if ( isLogined == false ) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(loginedUserId);
 
         if(user.isEmpty()){
             return null;
