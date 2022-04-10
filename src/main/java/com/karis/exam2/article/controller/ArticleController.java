9cd7040971568756e132ec9e196e7a9018f05019
karis1234 <qkzkf3487@naver.com>
2022-04-10 16:51:36 +0900

    글 작성시 로그인 여부 체크 및 common/js.html 도입

--- src/main/java/com/karis/exam2/article/controller/ArticleController.java
+++ src/main/java/com/karis/exam2/article/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -70,16 +71,44 @@ public class ArticleController {
     @ResponseBody
     public String doDelete(long id) {
         if (articleRepository.existsById(id) == false) {
-            return "%d번 게시물은 이미 삭제되었거나 존재하지 않습니다.".formatted(id);
+            return """
+                    <script>
+                    alert('%d번 게시물은 이미 삭제되었거나 존재하지 않습니다.');
+                    history.back();
+                    </script>
+                    """.formatted(id);
         }
 
         articleRepository.deleteById(id);
-        return "%d번 게시물이 삭제되었습니다.".formatted(id);
+        return """
+                <script>
+                alert('%d번 게시물이 삭제되었습니다.');
+                location.replace('list');
+                </script>
+                """
+                .formatted(id);
     }
 
     @RequestMapping("doWrite")
     @ResponseBody
-    public String doWrite(String title, String body) {
+    public String doWrite(String title, String body, HttpSession session) {
+        boolean isLogined = false;
+        long loginedUserId = 0;
+
+        if (session.getAttribute("loginedUserId") != null) {
+            isLogined = true;
+            loginedUserId = (long) session.getAttribute("loginedUserId");
+        }
+
+        if (isLogined == false) {
+            return """
+                    <script>
+                    alert('로그인 후 이용해주세요.');
+                    history.back();
+                    </script>
+                    """;
+        }
+
         if (title == null || title.trim().length() == 0) {
             return "제목을 입력해주세요.";
         }
@@ -97,7 +126,7 @@ public class ArticleController {
         article.setUpdateDate(LocalDateTime.now());
         article.setTitle(title);
         article.setBody(body);
-        User user = userRepository.findById(1L).get();
+        User user = userRepository.findById(loginedUserId).get();
         article.setUser(user);
 
         articleRepository.save(article);
@@ -111,7 +140,22 @@ public class ArticleController {
     }
 
     @RequestMapping("write")
-    public String showWrite() {
+    public String showWrite(HttpSession session, Model model) {
+        boolean isLogined = false;
+        long loginedUserId = 0;
+
+        if (session.getAttribute("loginedUserId") != null) {
+            isLogined = true;
+            loginedUserId = (long) session.getAttribute("loginedUserId");
+        }
+
+        System.out.println("isLogined : " + isLogined);
+
+        if (isLogined == false) {
+            model.addAttribute("msg", "로그인 후 이용해주세요.");
+            model.addAttribute("historyBack", true);
+            return "common/js";
+        }
         return "usr/article/write";
     }
 
